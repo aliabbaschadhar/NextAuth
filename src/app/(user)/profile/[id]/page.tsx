d3bbec02d1b3727cdd765f60669825891ab9198c
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-25 02:59:41 +0500

    userprofile done

---
+++ src/app/(user)/profile/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function UserProfile(params: { params: { id: string } }) {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-zinc-800">
+            <h1 className="text-white text-6xl font-sans">User Profile</h1>
+            <p className="text-black text-9xl font-mono bg-amber-400 p-5 m-4 rounded-e-full tracking-tighter ">{params.params.id}</p>
+        </div>
+    )
+
+}
---
+++ src/app/(user)/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-zinc-800">
+            <h1 className="text-white text-6xl font-sans">Profile</h1>
+        </div>
+    )
+}
