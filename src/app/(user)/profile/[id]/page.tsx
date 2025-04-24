24b20b35760d089abd5774cf0651e293a280c418
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-25 03:05:23 +0500

    userprofile done

--- src/app/(user)/profile/[id]/page.tsx
+++ src/app/(user)/profile/[id]/page.tsx
@@ -1,8 +1,8 @@
-export default function UserProfile(params: { params: { id: string } }) {
+export default async function UserProfile({ params }: { params: { id: string } }) {
     return (
         <div className="flex items-center justify-center min-h-screen bg-zinc-800">
             <h1 className="text-white text-6xl font-sans">User Profile</h1>
-            <p className="text-black text-9xl font-mono bg-amber-400 p-5 m-4 rounded-e-full tracking-tighter ">{params.params.id}</p>
+            <p className="text-black text-9xl font-mono bg-amber-400 p-5 m-4 rounded-e-full tracking-tighter ">{(await params).id}</p>
         </div>
     )
 
