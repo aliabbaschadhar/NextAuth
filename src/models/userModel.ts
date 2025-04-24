2166bb877ad52513c66d090a876597e2d537a365
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-25 03:31:55 +0500

    models done

---
+++ src/models/userModel.ts
@@ -0,0 +1,42 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true,
+        minlength: [3, "Username must be at least 3 characters"],
+        maxlength: [20, "Username must be at most 20 characters"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters"],
+    },
+    isVerfified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = model("user", userSchema);
+
+export {
+    User,
+}
