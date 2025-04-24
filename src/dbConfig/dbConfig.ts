bbbf3f194d5b8a3d871a0e2e55fd3f612873b6df
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-25 02:05:38 +0500

    DBconfig done

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL as string);
+        const connection = mongoose.connection;
+
+        connection.on("connected", () => {
+            console.log("MongoDB connected successfully!");
+        })
+
+        connection.on("error", (error) => {
+            console.log("MongoDB connection error:", error);
+            process.exit(1);
+        })
+    } catch (error) {
+        console.log("Something went wrong!")
+        console.error(error);
+    }
+}
